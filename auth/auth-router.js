7a458c962710b9fa206a5fe4cee21c063a0eb8ff
Jeremy Morales <jeremy.morales@gmail.com>
2019-08-31 09:48:37 -0700

    added authrouter code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,44 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
+const Auth = require('./authDb.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', async (req, res) => {
   // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  try {
+    const newUser = await Auth.add(user);
+    const token = generateToken(newUser);
+    res.status(201).json({ message: `Welcome ${user.username}`, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Error registering user' });
+  }
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+  try {
+    Auth.findbyFilter({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+
+          res.status(200).json({ message: `Welcome ${user.username}`, token });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
