ba1ae3dbbbdd1197978f3921aa1c450bea22f57b
Jeremy Morales <jeremy.morales@gmail.com>
2019-08-31 10:11:49 -0700

    added secrets and token generator

---
+++ auth/genToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
+module.exports = function(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '5m'
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+};
